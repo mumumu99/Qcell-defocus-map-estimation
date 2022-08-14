4732aaaf5e022809eba815ed4bbef14bfa8d571d
Muhyeon Kang <mumumu99@naver.com>
2022-08-14 15:44:36 +0000

    first commit

---
+++ dataset.py
@@ -0,0 +1,43 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+import numpy as np
+import matplotlib.pyplot as plt
+
+class QcellTrainDataset(Dataset):
+    def __init__(self, image_dir, transform=None):
+        self.image_dir = image_dir
+        self.images = os.listdir(image_dir)
+        self.images.sort()
+        self.label_list = list(range(-21,21,2))
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        image = np.zeros((111,111,4))
+        for i in range(4):
+            image_path = os.path.join(self.image_dir, self.images[index//4*4+i])
+            image[:,:,i] = plt.imread(image_path)
+        label = self.label_list[(int(self.images[index][0:5])+1)//500]
+
+        return image, label
+
+class QcellValDataset(Dataset):
+    def __init__(self, image_dir, transform=None):
+        self.image_dir = image_dir
+        self.images = os.listdir(image_dir)
+        self.images.sort()
+        self.label_list = list(range(-21,21,2))
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        image = np.zeros((111,111,4))
+        for i in range(4):
+            image_path = os.path.join(self.image_dir, self.images[index//4*4+i])
+            image[:,:,i] = plt.imread(image_path)
+        label = self.label_list[(int(self.images[index][0:5])+1)//50]
+
+        return image, label
---
+++ utils.py
@@ -0,0 +1,46 @@
+import torch
+import torchvision
+from dataset import QcellTrainDataset, QcellValDataset
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+
+def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
+    print("=> Saving checkpoint")
+    torch.save(state, filename)
+
+def load_checkpoint(checkpoint, model):
+    print("=> Loading checkpoint")
+    model.load_state_dict(checkpoint["state_dict"])
+
+def get_loaders(
+    train_dir,
+    val_dir,
+    batch_size,
+    num_workers=4,
+    pin_memory=True,
+):
+    train_ds = QcellTrainDataset(
+        image_dir=train_dir,
+    )
+
+    train_loader = DataLoader(
+        train_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=True,
+    )
+
+    val_ds = QcellValDataset(
+        image_dir=val_dir,
+    )
+
+    val_loader = DataLoader(
+        val_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=False,
+    )
+
+    return train_loader#, val_loader
