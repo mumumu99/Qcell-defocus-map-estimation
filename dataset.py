fba86587c1458352a0af1a71a50ecfd88cad6044
Muhyeon Kang <mumumu99@naver.com>
2022-08-15 10:49:58 +0000

    Edit __len__

--- dataset.py
+++ dataset.py
@@ -10,26 +10,32 @@ class QcellTrainDataset(Dataset):
         self.images = os.listdir(image_dir)
         self.images.sort()
         self.label_list = list(range(-21,21,2))
+        self.images_LT = []
+        for i in range(len(self.images)):
+            if i % 4 == 0:
+                self.images_LT.append(self.images[i])
 
     def __len__(self):
-        return len(self.images)
+        return len(self.images_LT)
 
     def __getitem__(self, index):
-        image_path = os.path.join(self.image_dir, self.images[index])
+        image_path = os.path.join(self.image_dir, self.images_LT[index])
         image = plt.imread(image_path)
         label = self.label_list[(int(self.images[index][0:5])+1)//500]
 
         return image, label
-    '''
+    
     def __getitem__(self, index):
         image = np.zeros((111,111,4))
-        for i in range(4):
-            image_path = os.path.join(self.image_dir, self.images[index//4*4+i])
+        image_path = os.path.join(self.image_dir, self.images_LT[index])
+        image[:,:,0] = plt.imread(image_path)
+        for i in range(1,4):
+            image_path = os.path.join(self.image_dir, self.images[index*4+i])
             image[:,:,i] = plt.imread(image_path)
         label = self.label_list[(int(self.images[index][0:5])+1)//500]
 
         return image, label
-    '''
+    
 
 class QcellValDataset(Dataset):
     def __init__(self, image_dir, transform=None):
